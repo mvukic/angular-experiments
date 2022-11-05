8e73dc25c0365e7181dfb30e42f51000b4131063
mvukic <matija.vukic2@gmail.com>
2022-11-05 19:35:13 +0100

    updates

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { App1Component } from './app/app-1.component';
-import { App2Component } from './app/app-2.component';
+import { bootstrapApplication } from '@angular/platform-browser'
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router'
+import { App1Component } from './app/app-1.component'
+import { App2Component } from './app/app-2.component'
 
-import routes from './app/routing/routes';
+import routes from './app/routing/routes'
 
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { httpInterceptor } from './app/http/interceptor';
-import { ApplicationRef } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
+import { httpInterceptor } from './app/http/interceptor'
+import { ApplicationRef } from '@angular/core'
 
 bootstrapApplication(window.innerWidth > 1000 ? App1Component : App2Component, {
-    providers: [provideRouter(routes), provideHttpClient(withInterceptors([httpInterceptor]))],
+  providers: [
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(withInterceptors([httpInterceptor]))],
 })
-    .then((appRef: ApplicationRef) => console.log(appRef.componentTypes))
-    .catch((err) => console.error(err));
+  .then((appRef: ApplicationRef) => console.log(appRef.componentTypes))
+  .catch((err) => console.error(err))
