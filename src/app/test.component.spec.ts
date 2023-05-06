8cba176dadc417153fe081a371ff36bf5ecb74eb
mvukic <matija.vukic2@gmail.com>
2023-05-06 17:17:50 +0200

    u

--- src/app/test.component.spec.ts
+++ src/app/test.component.spec.ts
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/angular';
 import { TestComponent, TestService } from './test.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { createComponentFactory } from '@ngneat/spectator/jest';
 
 describe('TestComponent 1', () => {
   it('should create 1', async () => {
@@ -18,25 +19,25 @@ describe('TestComponent 1', () => {
   });
 });
 
-// describe('TestComponent 2', () => {
-//   it('should create 2', async () => {
-//     const { fixture } = await render(TestComponent, { componentProviders: [TestService] });
-//     const component = fixture.componentInstance;
-//     expect(component.getName()).toEqual('TestService');
-//     expect(component.getAge()).toEqual(undefined);
-//   });
-// });
+describe('TestComponent 2', () => {
+  it('should create 2', async () => {
+    const { fixture } = await render(TestComponent, { componentProviders: [TestService] });
+    const component = fixture.componentInstance;
+    expect(component.getName()).toEqual('TestService');
+    expect(component.getAge()).toEqual(undefined);
+  });
+});
 
-// describe('TestComponent 3', () => {
-//   it('should create 3', async () => {
-//     const createComponent = createComponentFactory({
-//       component: TestComponent,
-//       componentProviders: [TestService],
-//     });
-//     const spectator = createComponent();
-//     const component = spectator.component;
-//     expect(component).toBeTruthy();
-//     expect(component.getName()).toEqual('TestService');
-//     expect(component.getAge()).toEqual(undefined);
-//   });
-// });
+describe('TestComponent 3', () => {
+  it('should create 3', async () => {
+    const createComponent = createComponentFactory({
+      component: TestComponent,
+      componentProviders: [TestService],
+    });
+    const spectator = createComponent();
+    const component = spectator.component;
+    expect(component).toBeTruthy();
+    expect(component.getName()).toEqual('TestService');
+    expect(component.getAge()).toEqual(undefined);
+  });
+});
