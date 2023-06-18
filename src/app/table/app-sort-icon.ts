6471544e42fc85cdcbca08b1b38c1072eef92cd1
mvukic <matija.vukic2@gmail.com>
2023-06-18 16:41:56 +0200

    u

---
+++ src/app/table/app-sort-icon.ts
@@ -0,0 +1,57 @@
+import { ChangeDetectionStrategy, Component, computed, EventEmitter, Input, Output, signal } from '@angular/core';
+import { SortDirection } from './table';
+
+@Component({
+  selector: 'app-sort-icon',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(click)': 'toggle()',
+  },
+  styles: [
+    `
+      span.blurred {
+        opacity: 0.2;
+        &:hover {
+          opacity: 0.7;
+        }
+      }
+    `,
+  ],
+  template: ` <span class="material-icons" [class.blurred]="!_active()">{{ _icon() }}</span> `,
+})
+export class AppSortIcon {
+  @Input()
+  name?: string;
+
+  @Input()
+  set active(value: boolean) {
+    this._active.set(value);
+  }
+
+  @Input()
+  set direction(value: SortDirection) {
+    this._direction.set(value);
+  }
+
+  @Output()
+  activeChange = new EventEmitter<boolean>();
+
+  @Output()
+  directionChange = new EventEmitter<SortDirection>();
+
+  readonly _active = signal<boolean>(false);
+  readonly _direction = signal<SortDirection>(1);
+
+  readonly _icon = computed(() => {
+    return this._direction() === 1 ? 'arrow_downward' : 'arrow_upward';
+  });
+
+  toggle() {
+    if (this._active()) {
+      this._direction.update((value) => (value === 1 ? -1 : 1));
+    }
+    this.directionChange.emit(this._direction());
+    this.activeChange.emit(this._active());
+  }
+}
--- src/app/table/table.ts
+++ src/app/table/table.ts
@@ -1,67 +1,24 @@
-import { ChangeDetectionStrategy, Component, computed, EventEmitter, Input, Output, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
 import { CdkTableModule } from '@angular/cdk/table';
+import { AppSortIcon } from './app-sort-icon';
 
-@Component({
-  selector: 'app-sort-icon',
-  standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  host: {
-    '(click)': 'toggle()',
-  },
-  styles: [
-    `
-      span.blurred {
-        opacity: 0.2;
-        &:hover {
-          opacity: 0.7;
-        }
-      }
-    `,
-  ],
-  template: `<span class="material-icons" [class.blurred]="!active">sort</span> `,
-})
-export class SortTrigger {
-  @Input({ required: true })
-  active = false;
-
-  @Output()
-  activeChange = new EventEmitter<boolean>();
-
-  toggle() {
-    this.activeChange.next(!this.active);
-  }
-}
-
-@Component({
-  selector: 'app-filter-column',
-  standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  template: `<input type="text" (keyup)="toggle($event)" />`,
-})
-export class FilterColumnInput {
-  @Input()
-  query!: string;
-
-  @Output()
-  queryChange = new EventEmitter<string>();
-
-  toggle({ target }: KeyboardEvent) {
-    this.queryChange.next((target as HTMLInputElement).value);
-  }
-}
+export type SortDirection = 1 | -1;
+export const columns = ['flag', 'position', 'name', 'weight', 'symbol'] as const;
+export type Attributes = (typeof columns)[number];
 
 export interface PeriodicElement {
   name: string;
   position: number;
   weight: number;
   symbol: string;
+  flag: boolean;
 }
 
 @Component({
   selector: 'table-example',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CdkTableModule, SortTrigger, FilterColumnInput],
+  imports: [CdkTableModule, AppSortIcon],
   styles: [
     `
       :host {
@@ -74,14 +31,22 @@ export interface PeriodicElement {
     `,
   ],
   template: `
+    <div>
+      Sort by: <b>{{ sort() }}</b> Sort direction: <b>{{ sortDirection() }}</b>
+    </div>
     <table cdk-table recycleRows fixedLayout [dataSource]="vm()">
       <!-- Position Column -->
+      <ng-container cdkColumnDef="flag">
+        <th cdk-header-cell *cdkHeaderCellDef>
+          Flag
+          <app-sort-icon [active]="sort() === 'flag'" (activeChange)="sort.set('flag')" (directionChange)="sortDirection.set($event)" />
+        </th>
+        <td cdk-cell *cdkCellDef="let element">{{ element.flag }}</td>
+      </ng-container>
       <ng-container cdkColumnDef="position">
         <th cdk-header-cell *cdkHeaderCellDef>
           No.
-          <app-sort-icon [active]="sort() === 'position'" (activeChange)="sort.set('position')" />
-          <br />
-          <app-filter-column [query]="filterByColumn()['position']" />
+          <app-sort-icon [active]="sort() === 'position'" (activeChange)="sort.set('position')" (directionChange)="sortDirection.set($event)" />
         </th>
         <td cdk-cell *cdkCellDef="let element">{{ element.position }}</td>
       </ng-container>
@@ -90,9 +55,7 @@ export interface PeriodicElement {
       <ng-container cdkColumnDef="name">
         <th cdk-header-cell *cdkHeaderCellDef>
           Name
-          <app-sort-icon [active]="sort() === 'name'" (activeChange)="sort.set('name')" />
-          <br />
-          <app-filter-column [query]="filterByColumn()['name']" />
+          <app-sort-icon name="name" [active]="sort() === 'name'" (activeChange)="sort.set('name')" (directionChange)="sortDirection.set($event)" />
         </th>
         <td cdk-cell *cdkCellDef="let element">{{ element.name }}</td>
       </ng-container>
@@ -101,7 +64,7 @@ export interface PeriodicElement {
       <ng-container cdkColumnDef="weight">
         <th cdk-header-cell *cdkHeaderCellDef>
           Weight
-          <app-sort-icon [active]="sort() === 'weight'" (activeChange)="sort.set('weight')" />
+          <app-sort-icon [active]="sort() === 'weight'" (activeChange)="sort.set('weight')" (directionChange)="sortDirection.set($event)" />
         </th>
         <td cdk-cell *cdkCellDef="let element">{{ element.weight }}</td>
       </ng-container>
@@ -110,7 +73,7 @@ export interface PeriodicElement {
       <ng-container cdkColumnDef="symbol">
         <th cdk-header-cell *cdkHeaderCellDef>
           Symbol
-          <app-sort-icon [active]="sort() === 'symbol'" (activeChange)="sort.set('symbol')" />
+          <app-sort-icon [active]="sort() === 'symbol'" (activeChange)="sort.set('symbol')" (directionChange)="sortDirection.set($event)" />
         </th>
         <td cdk-cell *cdkCellDef="let element">{{ element.symbol }}</td>
       </ng-container>
@@ -121,40 +84,39 @@ export interface PeriodicElement {
   `,
 })
 export default class Table {
-  columns = ['position', 'name', 'weight', 'symbol'] as const;
+  columns = columns;
 
   data = signal(DATA);
-  sort = signal<'position' | 'name' | 'weight' | 'symbol'>('name');
-  filterByColumn = signal({
-    position: '',
-    name: '',
-    weight: '',
-    symbol: '',
-  });
+  sort = signal<Attributes>('name');
+  sortDirection = signal<SortDirection>(1);
 
   vm = computed(() => {
-    return this.data()
-      .slice()
-      .sort((a, b) => compare(a, b, this.sort()));
+    const data = this.data();
+    const sortBy = this.sort();
+    const sortDirection = this.sortDirection();
+    return data.slice().sort((a, b) => compare(a, b, sortBy, sortDirection));
   });
 }
 
 const DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', flag: false },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', flag: true },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', flag: false },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', flag: false },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B', flag: false },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C', flag: true },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N', flag: true },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O', flag: true },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F', flag: false },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne', flag: true },
 ];
 
-function compare(a: PeriodicElement, b: PeriodicElement, sortBy: 'position' | 'name' | 'weight' | 'symbol'): number {
+function compare(a: PeriodicElement, b: PeriodicElement, sortBy: Attributes, direction: SortDirection): number {
   if (sortBy === 'weight' || sortBy === 'position') {
-    return a[sortBy] - b[sortBy];
+    return direction * (a[sortBy] - b[sortBy]);
   }
-  return a[sortBy].localeCompare(b[sortBy]);
+  if (sortBy === 'flag') {
+    return direction * (Number(b[sortBy]) - Number(a[sortBy]));
+  }
+  return direction * a[sortBy].localeCompare(b[sortBy]);
 }
