a3770259d48ab51c167fffdce6d5f8448b6eb8f6
mvukic <matija.vukic2@gmail.com>
2023-09-17 16:30:34 +0200

    u

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import routes from './app/routing/routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { httpInterceptor } from './app/http/interceptor';
@@ -9,7 +9,7 @@ import { LogEventPlugin } from './app/events/log-event-plugin.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
     provideHttpClient(withFetch(), withInterceptors([httpInterceptor])),
     provideAnimations(),
     {
