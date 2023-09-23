e227b51d2e0084cd82156d1f0417a1cf0c8b32b9
mvukic <matija.vukic2@gmail.com>
2023-09-23 15:33:51 +0200

    u

--- src/app/events/log-event-plugin.service.ts
+++ src/app/events/log-event-plugin.service.ts
@@ -6,19 +6,19 @@ import { EventManagerPlugin } from '@angular/platform-browser';
 export class LogEventPlugin extends EventManagerPlugin {
   #modifier = 'log';
 
-  constructor(@Inject(DOCUMENT) private doc: any) {
+  constructor(@Inject(DOCUMENT) doc: any) {
     super(doc);
   }
 
   override supports(event: string): boolean {
-    return event.endsWith(this.#modifier)
+    return event.endsWith(this.#modifier);
   }
 
   override addEventListener(element: HTMLElement, event: string, handler: Function): Function {
     const originalEvent = event
-    .split('.')
-    .filter((v) => v !== this.#modifier)
-    .join('.');
+      .split('.')
+      .filter((v) => v !== this.#modifier)
+      .join('.');
     const wrapped = (event: Event): void => {
       console.log(event, originalEvent, element);
       handler(event);
