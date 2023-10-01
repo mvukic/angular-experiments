1ae8ee1abf17b4f8e98a6cea6a271687bba96beb
mvukic <matija.vukic2@gmail.com>
2023-10-01 18:40:45 +0200

    u

--- src/app/new-structure-dirs.ts
+++ src/app/new-structure-dirs.ts
@@ -1,27 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'new-str-dirs',
   standalone: true,
+  imports: [CommonModule],
   template: `
-    <fieldset>
-      <legend>For loop</legend>
-      <ul>
-        <!-- @for (item of items(); track item) {
-        <li>{{ item }}</li>
-        } -->
-      </ul>
-    </fieldset>
-    <fieldset>
-      <legend>Deferred loading</legend>
-      @defer(on hover) {
-      <div>Defer block</div>
-      } @loading {
-      <div>Loading block</div>
-      } @placeholder {
-      <div>Placeholder block</div>
-      }
-    </fieldset>
+    <legend>Deferred loading</legend>
+    @defer(on hover) {
+    <div>Defer block</div>
+    } @loading {
+    <div>Loading block</div>
+    } @placeholder {
+    <div>Placeholder block</div>
+    }
   `,
 })
 export default class NewStructureDirs {
