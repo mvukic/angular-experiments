b8dfe1b49c1ac4b42552f4e8264ff91e76e8c9bb
mvukic <matija.vukic2@gmail.com>
2023-10-07 15:35:27 +0200

    u

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import routes from './routing/routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes, withHashLocation()),
+    provideClientHydration()
+],
 };
