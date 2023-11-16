aefcd567d891c0a386fe20a2e87385a0e5214618
mvukic <matija.vukic2@gmail.com>
2023-11-16 22:42:10 +0100

    u

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,9 @@ import {
 } from '@angular/router';
 
 import routes from './routing/routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withHashLocation(), withComponentInputBinding()),
-    provideClientHydration(),
   ],
 };
--- src/app/form-module.ts
+++ src/app/form-module.ts
@@ -2,53 +2,34 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { JsonPipe, NgForOf } from '@angular/common';
 
+export enum BoxType {
+  TYPE1 = 'TYPE1',
+  TYPE2 = 'TYPE2',
+}
+
 export type Box = {
   id: string;
   type: BoxType;
   name: string;
   opened: boolean;
   items: {
-    id: string;
+    id: number;
     name: string;
   }[];
 };
 
-// @Component({
-//   selector: 'form-items',
-//   standalone: true,
-//   imports: [FormsModule, NgForOf],
-//   template: `
-//     <div ngModelGroup="formGroup" #itemsFormGroup="NgModelGroup" style="display: flex; flex-direction: column; gap: 5px">
-//       <div *ngFor="let item of items">
-//         <span>Id: {{ item.id }} </span>
-//         <input [(ngModel)]="item.name" [name]="'itemName' + item.id" required />
-//       </div>
-//     </div>
-//   `,
-// })
-// export class ItemForm {
-//   formGroup = { items: [] };
-//   @Input()
-//   items: { id: string; name: string }[] = [];
-// }
-
-export enum BoxType {
-  TYPE1 = 'TYPE1',
-  TYPE2 = 'TYPE2',
-}
-
 @Component({
-  selector: 'form-module-example',
+  selector: 'form-box',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [FormsModule, NgForOf, JsonPipe],
   template: `
-    <fieldset>
-      <legend>Box</legend>
-      <form #f="ngForm">
+    <form #f="ngForm">
+      <fieldset>
+        <legend>Box</legend>
         <span>Id: {{ box.id }} </span>
-        <input [(ngModel)]="box.name" name="boxName" />
-        <select id="select" name="boxType" [(ngModel)]="box.type">
+        <input [(ngModel)]="box.name" name="name" />
+        <select id="select" name="type" [(ngModel)]="box.type">
           <option [value]="BoxType.TYPE1">Type 1</option>
           <option [value]="BoxType.TYPE2">Type 2</option>
         </select>
@@ -59,28 +40,64 @@ export enum BoxType {
           [(ngModel)]="box.opened"
         />
         <label for="opened">Opened</label>
-      </form>
-      <fieldset>
-        <legend>Items</legend>
-        <button (click)="addItem()">Add</button>
-        <div style="display: flex; flex-direction: column; gap: 5px">
-          @for (item of box.items; track item.id) {
-            <div>
-              <span>Id: {{ item.id }} </span>
-              <input
-                [(ngModel)]="item.name"
-                [name]="'itemName' + item.id"
-                required
-              />
-            </div>
-          }
-        </div>
+        <fieldset>
+          <legend>Items</legend>
+          <button (click)="addItem()">Add</button>
+          <div
+            style="display: flex; flex-direction: column; gap: 5px"
+            ngModelGroup="items"
+          >
+            @for (item of box.items; track item.id) {
+              <div>
+                <span>Id: {{ item.id }} </span>
+                <input
+                  [(ngModel)]="item.name"
+                  [name]="'item' + item.id"
+                  required
+                />
+                <button (click)="remove($index)">Remove</button>
+              </div>
+            }
+          </div>
+        </fieldset>
       </fieldset>
-    </fieldset>
-    <div><button (click)="save(f.value)">Save</button></div>
-    <pre>{{ f.value | json }}</pre>
-    <pre>{{ _box | json }}</pre>
+    </form>
+    <div style="display: flex;">
+      <div>
+        <span>Valid: {{ f.valid }}</span> <br />
+        <span>{{ f.errors | json }}</span> <br />
+        <pre>{{ f.value | json }}</pre>
+      </div>
+      <pre>{{ box | json }}</pre>
+    </div>
   `,
+})
+export class BoxForm {
+  @Input({ required: true, transform: copyAttribute })
+  box!: Box;
+
+  BoxType = BoxType;
+
+  addItem() {
+    const id = (this.box.items.at(-1)?.id ?? 0) + 1;
+    this.box.items.push({ id, name: `item ${id}` });
+  }
+
+  remove(index: number) {
+    this.box.items.splice(index, 1);
+  }
+}
+
+function copyAttribute(value: { [key: string]: any }): Record<string, any> {
+  return structuredClone(value);
+}
+
+@Component({
+  selector: 'form-module-example',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [BoxForm],
+  template: ` <form-box [box]="_box" /> `,
 })
 export default class FormModuleExample {
   _box: Box = {
@@ -89,23 +106,10 @@ export default class FormModuleExample {
     opened: true,
     type: BoxType.TYPE1,
     items: [
-      { id: '1', name: 'Name 1' },
-      { id: '2', name: 'Name 2' },
-      { id: '3', name: 'Name 3' },
-      { id: '4', name: 'Name 4' },
+      { id: 1, name: 'Name 1' },
+      { id: 2, name: 'Name 2' },
+      { id: 3, name: 'Name 3' },
+      { id: 4, name: 'Name 4' },
     ],
   };
-
-  /* The object might come from the outside */
-  box = this._box;
-  BoxType = BoxType;
-
-  addItem() {
-    this.box.items.push({ id: '5', name: 'item 5' });
-  }
-
-  save(value: any) {
-    console.log(value);
-    console.log(this._box);
-  }
 }
