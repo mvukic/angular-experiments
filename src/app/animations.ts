65fd66c92d3738f6d5ddd57e9f1c9dce5f23b3d9
mvukic <matija.vukic2@gmail.com>
2024-04-13 16:22:27 +0200

    u

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,5 +1,6 @@
 import { animate, style, transition, trigger } from '@angular/animations';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnInit, signal, viewChild } from '@angular/core';
+import { codeToHtml } from 'shiki';
 
 const showHide = trigger('fadeIn', [
   transition(':enter', [style({ opacity: 0 }), animate('.2s ease-out', style({ opacity: 1 }))]),
@@ -16,8 +17,44 @@ const showHide = trigger('fadeIn', [
     @if (show()) {
       <div @fadeIn>Some text</div>
     }
+
+    <div #htmlDiv></div>
+    <div #tsDiv></div>
   `,
 })
-export default class AnimationComponent {
+export default class AnimationComponent implements OnInit {
+  show = signal(false);
+  htmlDiv = viewChild.required('htmlDiv', { read: ElementRef });
+  tsDiv = viewChild.required('tsDiv', { read: ElementRef });
+
+  async ngOnInit() {
+    const html = await codeToHtml(html1, {
+      lang: 'angular-html',
+      theme: 'houston',
+    });
+    const ts = await codeToHtml(ts1, {
+      lang: 'typescript',
+      theme: 'houston',
+    });
+    this.htmlDiv().nativeElement.innerHTML = html;
+    this.tsDiv().nativeElement.innerHTML = ts;
+  }
+}
+
+const html1 = `
+<button (click)="show.set(!show())">Toggle</button>
+@if (show()) {
+  <div @fadeIn>Some text</div>
+}
+`;
+
+const ts1 = `
+@Component({
+  selector: '...',
+  standalone: true,
+  templateUrl: 'template.html',
+})
+export default class AnimationComponent implements OnInit {
   show = signal(false);
 }
+`;
