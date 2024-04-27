a2611f9f55ceffad8d5ed31cc5de55b436036e3a
mvukic <matija.vukic2@gmail.com>
2024-04-27 16:21:20 +0200

    u

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withHashLocation, withViewTransitions } from '@angular/router';
 
 import routes from './routing/routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withHashLocation(), withComponentInputBinding(), withViewTransitions()), provideAnimationsAsync()],
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes, withHashLocation(), withComponentInputBinding(), withViewTransitions()),
+    provideAnimationsAsync(),
+  ],
 };
